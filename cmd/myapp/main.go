6bbe9faaa630f6f5654d0a07fd915f711b479aab
John <Ivan_ksolo@mail.ru>
2024-11-17 21:08:23 +0300

    finish past 1

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,1 +1,16 @@
-package myapp
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Splucheviy/GoEchoFrameworkDDDCQRS/cmd/myapp/cli"
+)
+
+func main() {
+	if err := cli.Execute(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Some error occurred during execute app. Error: %v\n", err)
+
+		os.Exit(2)
+	}
+}
